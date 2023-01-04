a4a88b652868adf66e289bd4387444ec14fe0a3a
ao16blue <riko@ao16.blue>
2023-01-04 16:16:38 +0900

    aws

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+import { HeroLayout5Collection } from "./ui-components";
+import { AmpligramCollection } from "./ui-components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Amplify.configure(awsconfig);
+<ThemeProvider theme={studioTheme}>
+  <HeroLayout5Collection />
+  <AmpligramCollection />
+</ThemeProvider>;
